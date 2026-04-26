2240b813c2e2f6b8a5d9b7c33503276166ae1033
AleksaC <aleksacukovic1@gmail.com>
2026-04-27 01:51:31 +0200

    astro 6

--- website/src/utils/collections.ts
+++ website/src/utils/collections.ts
@@ -1,8 +1,8 @@
-import { getCollection, type AnyEntryMap } from "astro:content";
+import { getCollection, type DataEntryMap } from "astro:content";
 
-export async function getSortedCollection<C extends keyof AnyEntryMap>(
+export async function getSortedCollection<C extends keyof DataEntryMap>(
   collection: C
-): ReturnType<typeof getCollection<C>> {
+) {
   return (await getCollection(collection)).sort((a, b) =>
     a.data.date > b.data.date ? -1 : 1
   );
